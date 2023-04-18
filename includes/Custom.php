a94ec8883d0a80759fab202eb5169679e74b6c10
Tanner Moushey <tanner@missionlab.dev>
2023-04-18 17:36:13 -0500

    enable ccb chms connect

--- includes/Custom.php
+++ includes/Custom.php
@@ -43,7 +43,7 @@ class Custom {
 	 * @return void
 	 */
 	protected function actions() {
-//		add_filter( 'cp_connect_active_chms', function() {return 'pco';});
+		add_filter( 'cp_connect_active_chms', function() {return 'ccb';});
 //		add_action( 'tribe_events_single_event_after_the_content', [ $this, 'event_registration' ], 2 );
 //		add_filter( 'cp_connect_pco_event_args', [ $this, 'event_args' ] );
 //		add_filter( 'cp_connect_process_items', [ $this, 'filter_groups' ], 10, 2 );
@@ -56,15 +56,15 @@ class Custom {
 		if ( ! isset( $_GET['update-sermon-meta'] ) ) {
 			return;
 		}
-		
+
 		$sermons = get_posts( [ 'post_type' => 'cpl_item', 'posts_per_page' => 9999 ] );
-		
+
 		foreach( $sermons as $sermon ) {
 			$item = Item::get_instance_from_origin( $sermon->ID );
-			
+
 			if ( $audio = $item->get_meta_value( 'audio_url' ) ) {
 				 parse_str( parse_url( $audio )['query'], $results );
-				 
+
 				 if ( ! empty( $results['url'] ) ) {
 					 $item->update_meta_value( 'audio_url', $results['url'] );
 				 }
@@ -81,7 +81,7 @@ class Custom {
 	}
 	/**
 	 * Customize event details
-	 * 
+	 *
 	 * @param $args
 	 *
 	 * @return mixed
@@ -94,10 +94,10 @@ class Custom {
 		unset( $args['post_excerpt'] );
 		return $args;
 	}
-	
+
 	/**
 	 * Show registration button if registration is active
-	 * 
+	 *
 	 * @since  1.0.0
 	 *
 	 * @author Tanner Moushey
@@ -106,13 +106,13 @@ class Custom {
 		if ( ! $registration_url = get_post_meta( get_the_ID(), 'registration_url', true ) ) {
 			return;
 		}
-		
+
 		printf( '<div><a href="%s" class="cp-button is-large" target="_blank">Register Now</a></div>', $registration_url );
 	}
 
 	/**
 	 * Remove Unique Groups from Group import
-	 * 
+	 *
 	 * @param $items
 	 * @param $integration
 	 *
@@ -125,13 +125,13 @@ class Custom {
 		if ( 'groups' != $integration->type ) {
 			return $items;
 		}
-		
+
 		foreach( $items as $key => $item ) {
 			if ( in_array( 'Unique Groups', $item['group_type'] ) ) {
 				unset( $items[ $key ] );
 			}
 		}
-		
+
 		return $items;
 	}
 }
